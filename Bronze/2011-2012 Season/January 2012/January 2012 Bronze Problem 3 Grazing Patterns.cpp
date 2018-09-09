54c499842fba5dbe1d2bb5370d448e44a5adf71f
AmoleR <43121460+AmoleR@users.noreply.github.com>
2018-09-09 16:47:03 -0700

    Update January 2012 Bronze Problem 3 Grazing Patterns.cpp

--- Bronze/2011-2012 Season/January 2012/January 2012 Bronze Problem 3 Grazing Patterns.cpp
+++ Bronze/2011-2012 Season/January 2012/January 2012 Bronze Problem 3 Grazing Patterns.cpp
@@ -20,7 +20,7 @@ int K;                  //Number of Illegal Positions
 bool grassArea[5][5];	//Total Grass Area
 
 //Create a function to recursively count self-nonintersecting walks on this 5x5 board
-int selfNonIntersectingWalks(int start1, int start2, int remaining) {
+int selfNonIntersectingWalks(int start1, int start2) {
 	//Variable to return
 	int count;
 
@@ -28,10 +28,16 @@ int selfNonIntersectingWalks(int start1, int start2, int remaining) {
 	if (0 > start1 || 4 < start1 || 0 > start2 || 4 < start2) {
 		return 0;
 	}
+	
+	//First, we put this square we are on as barren
+	grassArea[start1][start2] = true;
+	
+	//We increment the number of barren lands by 1
+	K++;
 
-	//If we are at (5, 5), return 1 only if there are no remaining squares other than (5, 5)
+	//If we are at (5, 5), return 1 only if there are no remaining squares
 	if (start1 == 4 && start2 == 4) {
-		if (remaining == 1) {
+		if (K == 25) {
 			return 1;
 		}
 		return 0;
@@ -44,14 +50,14 @@ int selfNonIntersectingWalks(int start1, int start2, int remaining) {
 
 	//Otherwise, we have satisfied all conditions so far. Now, what we need to do is to do the recursion.
 
-	//First, we put this square we are on as barren
-	grassArea[start1][start2] = true;
-
-	//Then, we do the recursive sum
-	count = selfNonIntersectingWalks(start1 - 1, start2, remaining - 1) + selfNonIntersectingWalks(start1 + 1, start2, remaining - 1) + selfNonIntersectingWalks(start1, start2 - 1, remaining - 1) + selfNonIntersectingWalks(start1, start2 + 1, remaining - 1);
+	//We do the recursive sum
+	count = selfNonIntersectingWalks(start1 - 1, start2) + selfNonIntersectingWalks(start1 + 1, start2) + selfNonIntersectingWalks(start1, start2 - 1) + selfNonIntersectingWalks(start1, start2 + 1);
 
 	//Reset back to normal square
 	grassArea[start1][start2] = false;
+	
+	//We decrease the number of barren lands by 1
+	K++;
 
 	//Return the value
 	return count;
@@ -79,5 +85,5 @@ int main() {
 	}
 
 	//Print a self-nonintersecting walk from (1,  1) to (5, 5)
-	cout << selfNonIntersectingWalks(0, 0, (25 - K));
+	cout << selfNonIntersectingWalks(0, 0);
 }
