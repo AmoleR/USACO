81a30e976d7842e54c76a1188253e3fc565e15d6
AmoleR <43121460+AmoleR@users.noreply.github.com>
2018-09-11 19:43:54 -0700

    Update January 2012 Bronze Problem 1 Gifts.cpp

--- Bronze/2011-2012 Season/January 2012/January 2012 Bronze Problem 1 Gifts.cpp
+++ Bronze/2011-2012 Season/January 2012/January 2012 Bronze Problem 1 Gifts.cpp
@@ -1,10 +1,13 @@
 //USACO Bronze Division 2012 January Problem 1 Gifts
 /* How the program works
-	* Otherwise, we sort
+	* We have a struct of costs consisting of the gift and shipping costs
+	* We sort the array of costs
 	* Then we see if we can buy no gifts, even using the coupon; if so, we output 0
 	* If we can only buy the cheapest gift USING the coupon, we output 1
 	* Otherwise, we keep adding to our sum the cost; we note the breaking point as almostAnswer
+	* We store the maximum value of half the maximum gift cost in maximumDifferenceWithCouponAppliedBefore
 	* If we can apply the coupon, we output almostAnswer + 1
+	* If applying the coupon before allows us to add one more gift, we output almostAnswer + 1
 	* If not, we output almostAnswer
 */
 
@@ -13,12 +16,13 @@
 using namespace std;
 
 struct cost {
-	float gift, shipping;
+	int gift, shipping;
 };
 
 int N;                  //Number of Cows
 int B;                  //Total Budget
 cost costs[1000];		//Array of Gift and Shipping Costs
+int difference;			//Difference Between Budget and Different Cow's Costs (Used at End)
 int almostAnswer;       //Index of "Failed" Sum
 
 //Comparison for gifts
@@ -34,9 +38,6 @@ int main() {
 	//Input N and B
 	cin >> N >> B;
 
-	//Difference Between Budget and Different Cow's Costs (Used at End)
-	float difference;
-
 	for (int i = 0; i < N; i++) {
 		//Input the ith value of costs
 		cin >> costs[i].gift >> costs[i].shipping;
@@ -47,6 +48,9 @@ int main() {
 
 	//Reset the sum counter
 	difference = B;
+
+	//Maximum Difference when we apply the coupon before we go to the second loop
+	int maximumDifferenceWithCouponAppliedBefore = 0;
 
 	for (int i = 0; i < N; i++) {
 
@@ -61,27 +65,35 @@ int main() {
 
 		//The difference minus the new costs
 		difference -= costs[i].gift + costs[i].shipping;
+
+		//If the gift cost over 2 is greater than the previous maximum, rewrite it as the new maximum
+		if (costs[i].gift / 2 > maximumDifferenceWithCouponAppliedBefore)
+			maximumDifferenceWithCouponAppliedBefore = costs[i].gift / 2;
 	}
 
 	//We test all values after our breaking point
 	for (int i = almostAnswer; i < N; i++) {
 		//If applying the coupon keeps it below the budget
 		if (difference - costs[i].gift / 2 - costs[i].shipping >= 0) {
-
-			//Add 1 and print the answer
-			cout << almostAnswer + 1;
+			
+			//Add 1 to the answer
+			almostAnswer ++;
 
 			//Terminate the loop
 			break;
 		}
 
-		//If we have exhausted all of our options
-		else if (i == N - 1) {
-			//Write our answer
-			cout << almostAnswer;
+		//If we can apply the coupon before but still have positive difference
+		if (difference + maximumDifferenceWithCouponAppliedBefore - costs[i].gift - costs[i].shipping >= 0) {
 
-			//Break the loop in case
+			//Add 1 to the answer
+			almostAnswer ++;
+
+			//Terminate the loop
 			break;
 		}
 	}
+	
+	//Print the answer
+	cout << almostAnswer;
 }
